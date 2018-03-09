8c31450b952d382733870bb57d61451b3c1499b8
Mohini Agarwal <Mohini.Agarwal@NIIT-Tech.com>
2018-03-09 16:50:28 +0530

    Custom html helper created

---
+++ MusicStore/CustomHtmlHelper/InputHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MusicStore.CustomHtmlHelper
+{
+    public static class InputHelper
+    {
+        public static MvcHtmlString CustomInput<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression, object htmlAttributes)
+        {
+            
+            var name = ExpressionHelper.GetExpressionText(expression);
+            var metadata = ModelMetadata.FromLambdaExpression(expression, html.ViewData);
+            var objects = (htmlAttributes.ToString());
+            TagBuilder tb = new TagBuilder("input");
+            tb.Attributes.Add("name", name);
+            tb.Attributes.Add("value", metadata.Model as string);
+            tb.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+
+            return MvcHtmlString.Create(tb.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
